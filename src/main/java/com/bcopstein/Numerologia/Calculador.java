fe5685290b7ad70b8138698a58141eca143e8559
Larissa <larissa.magistrali@acad.pucrs.br>
2018-09-04 16:27:06 -0300

    commit teste

--- src/main/java/com/bcopstein/Numerologia/Calculador.java
+++ src/main/java/com/bcopstein/Numerologia/Calculador.java
@@ -2,6 +2,7 @@ package com.bcopstein.Numerologia;
 
 // Todos os metodos geram IllegalArgumentException em caso de caracter ilegal
 public class Calculador{
+	//Larissa
     private Formatador formatador;
     private Redutor reducao;
 
@@ -10,19 +11,25 @@ public class Calculador{
     public Calculador(Redutor reducao, Formatador formatador){
         this.reducao = reducao;
         this.formatador = formatador;
+        // -----------
     }
 
     public int calculaNumeroDaVida(String data){
+    	// http://www.dimension1111.com/numerology-life-path-number.html
         int numero = 0;
+        while(numero>9) {	
+        }
         return numero;
     }
 
     public int calculaNumeroDestino(String nomeCompleto){
+    	// http://www.dimension1111.com/numerology-destiny-number.html
         int numero = 0;
         return numero;
     }
-
+    
     public int calculaNumeroDesejosDaAlma(String nomeCompleto){
+    	// http://www.dimension1111.com/numerology-soul-urge-number.html
     	int numero = 0;
         return numero;
     }
