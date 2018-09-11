14b5edbbae157719a21e4ea66fb6ef719b643a46
GABRIEL VAZ DE SOUZA <17111238@pucrs.br>
2018-09-11 11:25:56 -0300

    Testes parametrizados

--- src/test/java/com/bcopstein/Numerologia/TesteFormatador.java
+++ src/test/java/com/bcopstein/Numerologia/TesteFormatador.java
@@ -12,42 +12,14 @@ import junitparams.Parameters;
 class TesteFormatador {
 	private Formatador f;
 	
-	
-	@Test
-	public void formataPalavraTest() {
+	@ParameterizedTest
+	@CsvSource({"teste, TESTE", 
+				"teste2, TESTE2",
+				"Teste.rt palavra, TESTERT PALAVRA",
+				"Teste formata frases, TESTE FORMATA FRASES",
+				"Testando 2formata frases, TESTANDO FRASES"})
+	public void formataPalavraTest(String str, String resp) {
 		f = new Formatador();
-		String str = "teste";
-		String str1 = "teste2,";
-		String str2 = "";
-		assertEquals("TESTEPALAVRA", f.formataPalavra(str));
-		assertEquals("TESTE2", f.formataPalavra(str1));
-		assertEquals(null, f.formataPalavra(str2));
-		
-		
-	}
-	
-	@Test
-	public void formataPalavraPlusTest() {
-		f = new Formatador();
-		String str = "teste1";
-		String str1 = "teste2/";
-		String str2 = "3teste";
-		String str3 = "";
-		assertEquals("TESTE1", f.formataPalavraPlus(str));
-		assertEquals("TESTE2", f.formataPalavraPlus(str1));
-		assertEquals(null, f.formataPalavraPlus(str2));
-		assertEquals(null, f.formataPalavraPlus(str3));
-	}
-	
-	@Test
-	public void formataFrasesTest() {
-		f = new Formatador();
-		String str = "Testando formata frases";
-		String str1 = "";
-		String str2 = "Testando 2formata frases";
-		assertEquals("TESTANDO FORMATA FRASES", f.formataFrase(str));
-		assertEquals(null, f.formataFrase(str1));
-		assertEquals("TESTANDO FRASES", f.formataFrase(str2));
-	}
-
+		assertEquals(resp, f.formataFrase(str));
+	}	
 }
