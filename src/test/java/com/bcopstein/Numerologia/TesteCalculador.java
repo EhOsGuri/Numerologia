f17ad62961ecf0e6f8588d989607909fcf6a6d01
17111233 <17111233@w3873874.portoalegre.pucrsnet.br>
2018-09-13 10:55:54 -0300

    testeCalculador

--- src/test/java/com/bcopstein/Numerologia/TesteCalculador.java
+++ src/test/java/com/bcopstein/Numerologia/TesteCalculador.java
@@ -2,43 +2,36 @@ package com.bcopstein.Numerologia;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import java.text.ParseException;
-
-import junitparams.Parameters;
 
 class TesteCalculador {
 	private Calculador c;
 	private Redutor r;
 	private Formatador f;
 	
-	@Test
-	@Parameters ({"05-07-1998,3","12/07/1997,9"})
+	@ParameterizedTest
+	@CsvSource({"12/07/1997,9"})
 	void testeNumDaVida(String resp,int esperado) throws ParseException {
 		c = new Calculador(r , f);
 		int obtido = c.calculaNumeroDaVida(resp);
 		assertEquals(obtido, esperado);
 		
 	}
-	@Test
-	@Parameters({"Larissa SM,3"})
+	@ParameterizedTest
+	@CsvSource({"Larissa SM,3"})
 	void testeNumeroDestino(String resp,int esperado) throws ParseException {
 		c=new Calculador(r, f);
 		int obtido = c.calculaNumeroDestino(resp);
 		assertEquals(obtido, esperado);
 	}
-	@Test
-	@Parameters({"Larissa SM,2"})
+	/*@ParameterizedTest
+	@CsvSource({"Larissa SM,2"})
 	void testeNumeroDesejosDaAlma(String resp,int esperado) throws ParseException {
 		c=new Calculador(r, f);
 		int obtido = c.calculaNumeroDesejosDaAlma(resp);
 		assertEquals(obtido, esperado);
-	}
+	}*/
 
 }
